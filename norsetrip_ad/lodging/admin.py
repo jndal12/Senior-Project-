96b51c449483489bd73bdf5434a92a2768279ff3
Dichha <raidi01@luther.edu>
2015-10-08 15:49:29 -0500

    adding lodging table

--- norsetrip_ad/lodging/admin.py
+++ norsetrip_ad/lodging/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Lodging
+
+#make the Lodging app modifiable in the admin
+admin.site.register(Lodging)
--- norsetrip_ad/lodging/models.py
+++ norsetrip_ad/lodging/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+ class Lodging(models.Model):
+ 	lodge_id = models.AutoField(primary_key = True, db_column = "LodgeId");#auto incrementing
+ 	lodge_name = models.CharField(max_length = 200)
+ 	#course_Id = models.ForeignKey(Course,verbose_name = "course_id")
+ 	lodge_address = models.CharField(max_length = 200, db_column = "Address")
+ 	city = models.CharField(max_length = 100, db_column = "City")
+ 	country = models.CharField(max_length = 100, db_column = "Country")
+ 	lodge_phone = models.IntegerField(db_column = "Phone")
+ 	lodge_email = models.EmailField(db_column = "Email")
+ 	lodge_description = models.TextField(db.column = "Description")
+
+
+
